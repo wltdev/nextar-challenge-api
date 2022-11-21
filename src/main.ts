9cb240b34ecb1e0e6b768fb6340533f3ee3b1284
Welinton Dias <mdias.welinton@gmail.com>
2022-11-21 12:50:52 -0300

    feat:http exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './HttpExceptions/http-exceptions.filters'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalFilters(new HttpExceptionFilter())
 
   const port = process.env.PORT || 3333
   await app.listen(port)
